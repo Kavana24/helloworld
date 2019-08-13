45b99d65148c8b58738c3a9a8cdd947ca2112bad
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-08-13 15:25:31 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.coveros.demo.helloworld;
 public class HelloWorld {
 
   public static void main(final String[] args) {
-    System.out.println("Hi Kavana you are amazing!");
+    System.out.println("Hi Kavana you are amazing!!!!!!!!!!!!!!!!!");
   }
 
 }
